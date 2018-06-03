ba380edeb40f8df0dbb7adc0d250c6adf4588098
ILikeBread7 <pjaworski47@gmail.com>
2018-06-03 23:54:44 +0200

    Added fabric.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import 'fabric';
+declare const fabric: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  private canvas;
+
+  ngOnInit(): void {
+    this.canvas = new fabric.Canvas('canvas', {
+      backgroundColor: '#fff',
+      selection: false
+    });
+    this.canvas.setWidth(300);
+    this.canvas.setHeight(500);
+
+    const rect = new fabric.Rect({
+      left: 100,
+      top: 100,
+      fill: 'red',
+      width: 20,
+      height: 20,
+      angle: 45
+    });
+
+    rect.on('modified', (e) => {
+      console.log(`Left: ${rect.left}, Top: ${rect.top}, ScaleX: ${rect.scaleX}, ScaleY: ${rect.scaleY}, Angle: ${rect.angle}`);
+    });
+
+    this.canvas.add(rect);
+  }
+
+  public toggleCanvasZoom(): void {
+    const zoom = this.canvas.getZoom();
+    this.canvas.setZoom(zoom === 1 ? 2 : 1);
+  }
+
 }
